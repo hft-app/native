ec8e143f93f841ebb796581513c62a990e099b9d
Lukas Jans <ljans@luniverse.de>
2019-08-30 17:02:43 +0200

    Remove installability
    In favor of launcher integration

--- script/client/menu.js
+++ script/client/menu.js
@@ -1,13 +1,3 @@
-// Register A2HS installer
-$(window).on('beforeinstallprompt', e => {
-	const $installer = $('.installer');
-	$installer.toggleClass('hidden', false);
-	$installer.on('click', () => {
-		$installer.toggleClass('hidden', true);
-		e.prompt();
-	});
-}, {once: true});
-
 // Logout confirmation
 $('.logout').on('click', e => {
 	if(!confirm('Möchtest du dich wirklich abmelden?\nDadurch werden alle deine Daten auf diesem Gerät gelöscht.')) e.preventDefault();
